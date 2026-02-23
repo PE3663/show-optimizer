6d984f9bb202cbf4547562652904eba38f479ea1
PE3663 <jim@pure-energydance.com>
2026-02-23 12:34:26 -0500

    v6: Fix Check-In report bug + Team back-to-back constraint

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -923,6 +923,8 @@ with tab4:
                 if routines:
                     first_pos, first_routine = routines[0]
                     checkin_data.append({'Performer': dancer, 'First Routine #': first_pos + 1, 'First Routine Name': first_routine})
+        df_checkin = pd.DataFrame(checkin_data)
+                    st.dataframe(df_checkin, use_container_width=True, hide_index=True)
             csv = df_checkin.to_csv(index=False)
             st.download_button("Download CSV", csv, f"{show['name']}_checkin.csv", "text/csv")
         elif report_type == "Check-Out":
@@ -1015,7 +1017,4 @@ with tab4:
             df_performer = pd.DataFrame(performer_data)
             csv = df_performer.to_csv(index=False)
             st.download_button("Download CSV", csv, f"{show['name']}_performer_schedules.csv", "text/csv")
-            df_checkin = pd.DataFrame(checkin_data)
-            st.dataframe(df_checkin, use_container_width=True, hide_index=True)
-            csv = df_checkin.to_csv(index=False)
-            st.download_button("Download CSV", csv, f"{show['name']}_checkin.csv", "text/csv")
+            
